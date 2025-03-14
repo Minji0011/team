c34b1476edce87341a00155eb75c213bd27dd3bd
MinjiKim0316 <minjikim.6420011@gmail.com>
2025-03-15 00:22:00 +0900

    senior changed js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let company = "Apple in Seattle, the United States";
 console.log(company);
 
-let job = "UI/US Frontend Software Engineer at Apple"
+let job = "UI/UX Frontend Software Engineer at Apple"
 console.log(job);
